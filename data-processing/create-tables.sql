4bcccf52c2f696456c6e50a07e0d0754d0cedd36
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-11-21 14:27:10 -0500

    Updating table strucutre for 2011 and 2012.

--- data-processing/create-tables.sql
+++ data-processing/create-tables.sql
@@ -1,8 +1,12 @@
 -- Create tables
+
+--------------------------------------------------
+-- 2011 Data
+
 -- Table: casual_subscriptions
 
-DROP TABLE IF EXISTS casual_subscriptions;
-CREATE TABLE casual_subscriptions
+DROP TABLE IF EXISTS casual_subscriptions_2011;
+CREATE TABLE casual_subscriptions_2011
 (
   id serial NOT NULL,
   number_rentals integer,
@@ -15,7 +19,7 @@ CREATE TABLE casual_subscriptions
   activation_end_date timestamp without time zone,
   gc_friendly_name character varying(128),
   bikes integer,
-  CONSTRAINT casual_subscriptions_id PRIMARY KEY (id )
+  CONSTRAINT casual_subscriptions_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
@@ -23,8 +27,8 @@ WITH (
 
 -- Table: rentals
 
-DROP TABLE IF EXISTS rentals;
-CREATE TABLE rentals
+DROP TABLE IF EXISTS rentals_2011;
+CREATE TABLE rentals_2011
 (
   id serial NOT NULL,
   rental_id character varying(16),
@@ -43,7 +47,7 @@ CREATE TABLE rentals
   subscription_type_a character varying(128),
   subscription_type_b character varying(128),
   birth_date date,
-  CONSTRAINT rentals_id PRIMARY KEY (id )
+  CONSTRAINT rentals_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
@@ -51,8 +55,8 @@ WITH (
 
 -- Table: subscribers
 
-DROP TABLE IF EXISTS subscribers;
-CREATE TABLE subscribers
+DROP TABLE IF EXISTS subscribers_2011;
+CREATE TABLE subscribers_2011
 (
   id serial NOT NULL,
   gender character varying(8),
@@ -69,7 +73,7 @@ CREATE TABLE subscribers
   birth_date date,
   start_date timestamp without time zone,
   end_date timestamp without time zone,
-  CONSTRAINT subscribers_id PRIMARY KEY (id )
+  CONSTRAINT subscribers_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
@@ -77,8 +81,8 @@ WITH (
 
 -- Table: stations
 
-DROP TABLE IF EXISTS stations;
-CREATE TABLE stations
+DROP TABLE IF EXISTS stations_2011;
+CREATE TABLE stations_2011
 (
   id serial NOT NULL,
   terminal_id character varying(16),
@@ -87,49 +91,49 @@ CREATE TABLE stations
   lat numeric,
   lon numeric,
   install_date date,
-  CONSTRAINT stations_id PRIMARY KEY (id )
+  CONSTRAINT stations_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
 );
-SELECT AddGeometryColumn('stations', 'location_geom', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('stations_2011', 'location_geom', 4326, 'POINT', 2);
 
 
 -- Table: routes
 
-DROP TABLE IF EXISTS routes;
-CREATE TABLE routes
+DROP TABLE IF EXISTS routes_2011;
+CREATE TABLE routes_2011
 (
   id serial NOT NULL,
   terminal_id_start character varying(16),
   terminal_id_end character varying(16),
-  CONSTRAINT routes_id PRIMARY KEY (id )
+  CONSTRAINT routes_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
 );
-SELECT AddGeometryColumn('routes', 'start_geom', 4326, 'POINT', 2);
-SELECT AddGeometryColumn('routes', 'end_geom', 4326, 'POINT', 2);
-SELECT AddGeometryColumn('routes', 'route_geom', 4326, 'LINESTRING', 2);
+SELECT AddGeometryColumn('routes_2011', 'start_geom', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('routes_2011', 'end_geom', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('routes_2011', 'route_geom', 4326, 'LINESTRING', 2);
 
 -- Table: average_day
-DROP TABLE IF EXISTS average_day;
-CREATE TABLE average_day
+DROP TABLE IF EXISTS average_day_2011;
+CREATE TABLE average_day_2011
 (
   id serial NOT NULL,
   start_time time without time zone,
   end_time time without time zone,
   total integer,
   average numeric,
-  CONSTRAINT average_day_id PRIMARY KEY (id )
+  CONSTRAINT average_day_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
 );
 
 -- Table: average_all_days
-DROP TABLE IF EXISTS average_all_days;
-CREATE TABLE average_all_days
+DROP TABLE IF EXISTS average_all_days_2011;
+CREATE TABLE average_all_days_2011
 (
   id serial NOT NULL,
   day date,
@@ -137,8 +141,109 @@ CREATE TABLE average_all_days
   end_time time without time zone,
   total integer,
   difference numeric,
-  CONSTRAINT average_all_days_id PRIMARY KEY (id )
+  CONSTRAINT average_all_days_2011_id PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
 );
+
+--------------------------------------------------
+-- 2012 Data
+
+-- Table: rentals
+
+DROP TABLE IF EXISTS rentals_2012;
+CREATE TABLE rentals_2012
+(
+  id serial NOT NULL,
+  rental_id character varying(16),
+  account_number character varying(32),
+  subscriber_id character varying(32),
+  start_date timestamp without time zone,
+  start_station character varying(128),
+  start_terminal character varying(16),
+  end_date timestamp without time zone,
+  end_station character varying(128),
+  end_terminal character varying(16),
+  duration_seconds numeric,
+  bike_id character varying(16),
+  gender character varying(8),
+  zip character varying(16),
+  subscription_type_a character varying(128),
+  subscription_type_b character varying(128),
+  birth_date date,
+  CONSTRAINT rentals_2012_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Table: stations
+
+DROP TABLE IF EXISTS stations_2012;
+CREATE TABLE stations_2012
+(
+  id serial NOT NULL,
+  terminal_id character varying(16),
+  common_name character varying(128),
+  station character varying(128),
+  lat numeric,
+  lon numeric,
+  install_date date,
+  bike_docks numeric,
+  CONSTRAINT stations_2012_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+SELECT AddGeometryColumn('stations_2012', 'location_geom', 4326, 'POINT', 2);
+
+
+-- Table: routes
+
+DROP TABLE IF EXISTS routes_2012;
+CREATE TABLE routes_2012
+(
+  id serial NOT NULL,
+  terminal_id_start character varying(16),
+  terminal_id_end character varying(16),
+  CONSTRAINT routes_2012_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+SELECT AddGeometryColumn('routes_2012', 'start_geom', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('routes_2012', 'end_geom', 4326, 'POINT', 2);
+SELECT AddGeometryColumn('routes_2012', 'route_geom', 4326, 'LINESTRING', 2);
+
+-- Table: average_day
+DROP TABLE IF EXISTS average_day_2012;
+CREATE TABLE average_day_2012
+(
+  id serial NOT NULL,
+  start_time time without time zone,
+  end_time time without time zone,
+  total integer,
+  average numeric,
+  CONSTRAINT average_day_2012_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Table: average_all_days
+DROP TABLE IF EXISTS average_all_days_2012;
+CREATE TABLE average_all_days_2012
+(
+  id serial NOT NULL,
+  day date,
+  start_time time without time zone,
+  end_time time without time zone,
+  total integer,
+  difference numeric,
+  CONSTRAINT average_all_days_2012_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+
